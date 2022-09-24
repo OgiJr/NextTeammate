a2ea383c401e807ee4e26dfcab955cc88e80c31b
Ognian Trajanov Jr <ogtrjr@gmail.com>
2022-09-24 14:00:28 -0400

    logbuttons

--- src/layout/LogButton.js
+++ src/layout/LogButton.js
@@ -11,11 +11,15 @@ export const LogButton = () => {
   return isLoggedIn ? (
     <div className="flex flex-row justify-center gap-2 flex-wrap">
       <Link href="/dashboard-redirector">
-        <Button variant="success">
+        <Button
+          variant="success"
+          className="thm-btn bg-thm-color-three thm-color-two-shadow btn-rounded mr-4 mb-4 wow fadeInRight"
+        >
           Dashboard <i className="fal fa-chevron-right ml-2" />
         </Button>
       </Link>
       <Button
+        className="thm-btn bg-thm-color-five thm-color-two-shadow btn-rounded mr-4 mb-4 wow fadeInRight"
         variant="danger"
         onClick={async () => {
           await fetch("/api/logout");
@@ -26,13 +30,13 @@ export const LogButton = () => {
       </Button>
     </div>
   ) : (
-    <Button
-      variant="primary"
-      onClick={() => {
-        router.push("/login");
-      }}
-    >
-      Log in <i className="fal fa-chevron-right ml-2" />
-    </Button>
+    <Link href="/login">
+      <a
+        className="thm-btn bg-thm-color-three thm-color-two-shadow btn-rounded mr-4 mb-4 wow fadeInRight"
+        data-wow-delay=".50ms"
+      >
+        Log in <i className="fal fa-chevron-right ml-2" />{" "}
+      </a>
+    </Link>
   );
 };
