08452602a482b158c895ac63f2e7b8fe071831e9
0xWANT3D <0xwant3d@gmail.com>
2022-10-25 20:12:24 +0100

    fix

--- pages/dashboard-user.js
+++ pages/dashboard-user.js
@@ -5,7 +5,7 @@ import { Button } from "react-bootstrap";
 import { authCookie } from "../lib/cookies";
 import PageTitleBanner from "../src/components/PageTitleBanner";
 import Layout from "../src/layout/Layout";
-import { dbConnect, dbUserToIronUser, getIronUserWorkStats, isIronUserWorking } from "../lib/db";
+import { dbConnect, dbUserToIronUser, getIronUserWorkStats, isIronUserAssigned, isIronUserWorking } from "../lib/db";
 import User from "../models/User";
 import Footer from "../src/layout/Footer";
 import { useRouter } from "next/router";
@@ -17,6 +17,7 @@ const DashboardUser = ({
   work_hours_this_month,
   average_hours_per_week,
   projected_salary,
+  is_assigned,
 }) => {
   const router = useRouter();
 
@@ -40,86 +41,90 @@ const DashboardUser = ({
               <div className="team_text pl-0 pl-xl-5 pl-lg-3 ">
                 <h3 className="name wow fadeInDown">{user.first_name + " " + user.last_name}</h3>
                 <p className="desi thm-color-two wow fadeInUp">{user.bio}</p>
-                <ul className="info wow fadeInDown">
-                  <li>
-                    <i className="icon fal fa-clock bg-thm-color-three" />
-                    <div className="text">
-                      <h6 className="mb-0">Expected hours per week</h6>
-                      <p className="mb-0">{user.work_data.expected_hours_weekly}</p>
+                {is_assigned ? (
+                  <ul className="info wow fadeInDown">
+                    <li>
+                      <i className="icon fal fa-clock bg-thm-color-three" />
+                      <div className="text">
+                        <h6 className="mb-0">Expected hours per week</h6>
+                        <p className="mb-0">{user.work_data.expected_hours_weekly}</p>
+                      </div>
+                    </li>
+                    <li>
+                      <i className="icon fal fa-lightbulb-on" />
+                      <div className="text">
+                        <h6 className="mb-0">Work hours this week</h6>
+                        <p className="mb-0">{work_hours_this_week}</p>
+                      </div>
+                    </li>
+                    <li>
+                      <i className="icon fal fa-calendar-week" />
+                      <div className="text">
+                        <h6 className="mb-0">Average hours per week</h6>
+                        <p className="mb-0">{average_hours_per_week}</p>
+                      </div>
+                    </li>
+                    <li>
+                      <i className="icon fal fa-calendar" />
+                      <div className="text">
+                        <h6 className="mb-0">Work hours this month</h6>
+                        <p className="mb-0">{work_hours_this_month}</p>
+                      </div>
+                    </li>
+                    <li>
+                      <i className="icon fal fa-dollar-sign" />
+                      <div className="text">
+                        <h6 className="mb-0">Projected Salary *</h6>
+                        <p className="mb-0">
+                          {projected_salary} {user.work_data.currency}
+                        </p>
+                      </div>
+                    </li>
+                    <div className="flex flex-col gap-4">
+                      <div className="flex flex-row items-center justify-center">
+                        {is_working ? (
+                          <Button
+                            variant="danger"
+                            className="px-4 text-2xl"
+                            onClick={async () => {
+                              await fetch("/api/clock-out");
+                              router.reload();
+                            }}
+                          >
+                            Clock Out
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="success"
+                            disabled={!user.work_data.current_price_per_hour}
+                            className="px-4 text-2xl"
+                            onClick={async () => {
+                              await fetch("/api/clock-in");
+                              router.reload();
+                            }}
+                          >
+                            Clock In
+                          </Button>
+                        )}
+                      </div>
+                      <div className="flex flex-row items-center justify-center gap-4">
+                        <Link href="/edit-user">
+                          <Button variant="dark" className="px-4 min-w-[25%]">
+                            Edit Account
+                          </Button>
+                        </Link>
+                        <Link href="/set-picture">
+                          <Button variant="dark" className="px-4 min-w-[25%]">
+                            Change Picture
+                          </Button>
+                        </Link>
+                      </div>
+                      <span>* This is a projection based on estimates and past performance, not a promise.</span>
                     </div>
-                  </li>
-                  <li>
-                    <i className="icon fal fa-lightbulb-on" />
-                    <div className="text">
-                      <h6 className="mb-0">Work hours this week</h6>
-                      <p className="mb-0">{work_hours_this_week}</p>
-                    </div>
-                  </li>
-                  <li>
-                    <i className="icon fal fa-calendar-week" />
-                    <div className="text">
-                      <h6 className="mb-0">Average hours per week</h6>
-                      <p className="mb-0">{average_hours_per_week}</p>
-                    </div>
-                  </li>
-                  <li>
-                    <i className="icon fal fa-calendar" />
-                    <div className="text">
-                      <h6 className="mb-0">Work hours this month</h6>
-                      <p className="mb-0">{work_hours_this_month}</p>
-                    </div>
-                  </li>
-                  <li>
-                    <i className="icon fal fa-dollar-sign" />
-                    <div className="text">
-                      <h6 className="mb-0">Projected Salary *</h6>
-                      <p className="mb-0">
-                        {projected_salary} {user.work_data.currency}
-                      </p>
-                    </div>
-                  </li>
-                  <div className="flex flex-col gap-4">
-                    <div className="flex flex-row items-center justify-center">
-                      {is_working ? (
-                        <Button
-                          variant="danger"
-                          className="px-4 text-2xl"
-                          onClick={async () => {
-                            await fetch("/api/clock-out");
-                            router.reload();
-                          }}
-                        >
-                          Clock Out
-                        </Button>
-                      ) : (
-                        <Button
-                          variant="success"
-                          disabled={!user.work_data.current_price_per_hour}
-                          className="px-4 text-2xl"
-                          onClick={async () => {
-                            await fetch("/api/clock-in");
-                            router.reload();
-                          }}
-                        >
-                          Clock In
-                        </Button>
-                      )}
-                    </div>
-                    <div className="flex flex-row items-center justify-center gap-4">
-                      <Link href="/edit-user">
-                        <Button variant="dark" className="px-4 min-w-[25%]">
-                          Edit Account
-                        </Button>
-                      </Link>
-                      <Link href="/set-picture">
-                        <Button variant="dark" className="px-4 min-w-[25%]">
-                          Change Picture
-                        </Button>
-                      </Link>
-                    </div>
-                    <span>* This is a projection based on estimates and past performance, not a promise.</span>
-                  </div>
-                </ul>
+                  </ul>
+                ) : (
+                  <div className="text-[#ff0000] text-xl"> Please ask the administrator to assign work data! </div>
+                )}
               </div>
             </div>
           </div>
@@ -165,6 +170,7 @@ export const getServerSideProps = withIronSessionSsr(async function getServerSid
       return {
         props: {
           user: req.session.user,
+          is_assigned: isIronUserAssigned(req.session.user),
           is_working: isIronUserWorking(req.session.user),
           work_hours_this_week,
           work_hours_this_month,
