221eebdc4ddee3a3cc333bfc8d357a4d160329d9
Ognian Trajanov Jr <ogtrjr@gmail.com>
2022-11-18 09:28:26 -0500

    company

--- pages/dashboard-records.js
+++ pages/dashboard-records.js
@@ -7,14 +7,7 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { authCookie } from "../lib/cookies";
 import Footer from "../src/layout/Footer";
-import {
-  Col,
-  Text,
-  Row,
-  User as NextUser,
-  Table,
-  Input,
-} from "@nextui-org/react";
+import { Col, Text, Row, User as NextUser, Table, Input } from "@nextui-org/react";
 import { StyledBadge } from "../src/components/zoom-panel/StyledBadge";
 import useSWR, { useSWRConfig } from "swr";
 
@@ -31,30 +24,17 @@ const DashboardAdmin = ({ start, end }) => {
     { name: "STATUS", uid: "status" },
     { name: "WORK HOURS", uid: "hoursActual" },
     { name: "PERFORMANCE", uid: "performance" },
+    { name: "COMPANY", uid: "company" },
     { name: "SALARY", uid: "salary" },
   ];
 
   const [start_date, set_start_date] = React.useState(new Date(start));
   const [end_date, set_end_date] = React.useState(new Date(end));
 
-  const isDate = (date) =>
-    new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
+  const isDate = (date) => new Date(date) !== "Invalid Date" && !isNaN(new Date(date));
 
-  const fetcher = (url, queryParams = "") =>
-    fetch(`${url}${queryParams}`).then(async (res) => await res.json());
-  const { data } = useSWR(
-    [
-      "/api/get-records",
-      `?start=${
-        isDate(start_date)
-          ? start_date.toISOString()
-          : new Date(0).toISOString()
-      }&end=${
-        isDate(end_date) ? end_date.toISOString() : new Date().toISOString()
-      }`,
-    ],
-    fetcher
-  );
+  const fetcher = (url, queryParams = "") => fetch(`${url}${queryParams}`).then(async (res) => await res.json());
+  const { data } = useSWR(["/api/get-records", `?start=${isDate(start_date) ? start_date.toISOString() : new Date(0).toISOString()}&end=${isDate(end_date) ? end_date.toISOString() : new Date().toISOString()}`], fetcher);
 
   const renderCell = (user, columnKey) => {
     switch (columnKey) {
@@ -64,6 +44,8 @@ const DashboardAdmin = ({ start, end }) => {
             {user.email}
           </NextUser>
         );
+      case "company":
+        return <div className="font-bold">Microsoft</div>;
       case "hoursActual":
         return user.is_assigned ? (
           <Col>
@@ -82,13 +64,7 @@ const DashboardAdmin = ({ start, end }) => {
           <></>
         );
       case "status":
-        return (
-          <StyledBadge
-            type={user.is_assigned && user.status ? "active" : "paused"}
-          >
-            {user.is_assigned && user.status ? "Clocked In" : "Clocked Out"}
-          </StyledBadge>
-        );
+        return <StyledBadge type={user.is_assigned && user.status ? "active" : "paused"}>{user.is_assigned && user.status ? "Clocked In" : "Clocked Out"}</StyledBadge>;
       case "salary":
         return user.is_assigned ? (
           <Col>
@@ -104,15 +80,7 @@ const DashboardAdmin = ({ start, end }) => {
           <></>
         );
       case "performance":
-        return (
-          <StyledBadge
-            type={user.actual_work >= user.expected_work ? "active" : "paused"}
-          >
-            {user.actual_work >= user.expected_work
-              ? "OK Performance"
-              : "Underperforming"}
-          </StyledBadge>
-        );
+        return <StyledBadge type={user.actual_work >= user.expected_work ? "active" : "paused"}>{user.actual_work >= user.expected_work ? "OK Performance" : "Underperforming"}</StyledBadge>;
       default:
         return <div className="font-bold"></div>;
     }
@@ -122,12 +90,7 @@ const DashboardAdmin = ({ start, end }) => {
       <div className="flex flex-row min-w-full bg-gradient-to-r from-cyan-500 to-blue-500 justify-between items-center px-10">
         <div className="flex flex-row justify-center my-2">
           <Link href="/dashboard-user">
-            <Image
-              src="/assets/images/nextlogowhite.png"
-              width={100}
-              height={100}
-              layout="fixed"
-            />
+            <Image src="/assets/images/nextlogowhite.png" width={100} height={100} layout="fixed" />
           </Link>
         </div>
         <div className=" flex flex-col mt-3 md:mt-0 md:flex-row justify-evenly md:gap-8">
@@ -159,17 +122,11 @@ const DashboardAdmin = ({ start, end }) => {
             aria-label="start-date"
             width="186px"
             type="date"
-            value={`${start_date.getFullYear()}-${pad(
-              start_date.getMonth() + 1
-            )}-${pad(start_date.getDate())}`}
+            value={`${start_date.getFullYear()}-${pad(start_date.getMonth() + 1)}-${pad(start_date.getDate())}`}
             onChange={(e) => {
               e.preventDefault();
 
-              const new_date = new Date(
-                parseInt(e.target.value.substring(0, 4)),
-                parseInt(e.target.value.substring(5, 7)),
-                parseInt(e.target.value.substring(8, 10))
-              );
+              const new_date = new Date(parseInt(e.target.value.substring(0, 4)), parseInt(e.target.value.substring(5, 7)), parseInt(e.target.value.substring(8, 10)));
 
               if (new_date >= new Date()) {
                 return;
@@ -185,17 +142,11 @@ const DashboardAdmin = ({ start, end }) => {
             aria-label="end-date"
             width="186px"
             type="date"
-            value={`${end_date.getFullYear()}-${pad(
-              end_date.getMonth() + 1
-            )}-${pad(end_date.getDate())}`}
+            value={`${end_date.getFullYear()}-${pad(end_date.getMonth() + 1)}-${pad(end_date.getDate())}`}
             onChange={(e) => {
               e.preventDefault();
 
-              const new_date = new Date(
-                parseInt(e.target.value.substring(0, 4)),
-                parseInt(e.target.value.substring(5, 7)),
-                parseInt(e.target.value.substring(8, 10))
-              );
+              const new_date = new Date(parseInt(e.target.value.substring(0, 4)), parseInt(e.target.value.substring(5, 7)), parseInt(e.target.value.substring(8, 10)));
 
               if (new_date < start_date) {
                 return;
@@ -216,11 +167,7 @@ const DashboardAdmin = ({ start, end }) => {
       >
         <Table.Header columns={columns}>
           {(column) => (
-            <Table.Column
-              key={column.uid}
-              hideHeader={column.uid === "actions"}
-              align={column.uid === "actions" ? "center" : "start"}
-            >
+            <Table.Column key={column.uid} hideHeader={column.uid === "actions"} align={column.uid === "actions" ? "center" : "start"}>
               {column.name}
             </Table.Column>
           )}
@@ -228,57 +175,44 @@ const DashboardAdmin = ({ start, end }) => {
         {
           //Да провери дали employee-тата са в същата компания или си админ
         }
-        <Table.Body items={!data ? [] : data.data}>
-          {(item) => (
-            <Table.Row key={item.email}>
-              {(columnKey) => (
-                <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>
-              )}
-            </Table.Row>
-          )}
-        </Table.Body>
+        <Table.Body items={!data ? [] : data.data}>{(item) => <Table.Row key={item.email}>{(columnKey) => <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>}</Table.Row>}</Table.Body>
       </Table>
       <Footer />
     </div>
   );
 };
 
-export const getServerSideProps = withIronSessionSsr(
-  async function getServerSideProps({ req, query }) {
-    const user = req.session.user;
+export const getServerSideProps = withIronSessionSsr(async function getServerSideProps({ req, query }) {
+  const user = req.session.user;
 
-    if (!user) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/login",
-        },
-        props: {},
-      };
-    }
-
-    if (!user.is_admin) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/dashboard-user",
-        },
-        props: {},
-      };
-    }
-
-    const props = {
-      start: query.start
-        ? query.start
-        : new Date(Date.now() - 60 * 60 * 24 * 30 * 1000).toISOString(),
-      end: query.end ? query.end : new Date().toISOString(),
-    };
-
+  if (!user) {
     return {
-      props,
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+      props: {},
     };
-  },
-  authCookie
-);
+  }
+
+  if (!user.is_admin) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/dashboard-user",
+      },
+      props: {},
+    };
+  }
+
+  const props = {
+    start: query.start ? query.start : new Date(Date.now() - 60 * 60 * 24 * 30 * 1000).toISOString(),
+    end: query.end ? query.end : new Date().toISOString(),
+  };
+
+  return {
+    props,
+  };
+}, authCookie);
 
 export default DashboardAdmin;
