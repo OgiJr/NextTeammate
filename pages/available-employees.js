02e6fdb3a95bf4c45105dac58061a76f94277e16
Boris Radulov <sc20br@leeds.ac.uk>
2022-12-26 18:02:49 +0200

    email fix

--- pages/available-employees.js
+++ pages/available-employees.js
@@ -188,8 +188,6 @@ export const getServerSideProps = withIronSessionSsr(async function getServerSid
 
   let others = await Promise.all(my_others.map(async (e) => await dbUserToIronUser(e)));
 
-  others = others.filter((x) => x.password);
-
   return {
     props: {
       user,
