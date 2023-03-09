0005b3eed1ce78d67b24e25e091e129ed9fde1ac
Boris Radulov <sc20br@leeds.ac.uk>
2023-03-09 00:20:24 +0000

    og hom

--- src/layout/ScrollTop.js
+++ src/layout/ScrollTop.js
@@ -1,10 +1,6 @@
 import React from "react";
 
 const ScrollTop = () => {
-  const topFunction = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  };
   return <></>;
 };
 
