e1f89dae392838911962181fdddd67cd9b4c07e1
Boris Radulov <sc20br@leeds.ac.uk>
2023-03-29 22:34:02 +0300

    no

--- next.config.js
+++ next.config.js
@@ -2,49 +2,6 @@ const withTM = require("next-transpile-modules")(["@jitsi/react-sdk"]); // pass 
 
 module.exports = withTM({
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "nextteammate.com",
-        port: "80",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "nextteammate.com",
-        port: "443",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "nextteammate.com",
-        port: "3001",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/**",
-      },
-    ],
+    domains: ["localhost", "nextteammate.com"],
   },
 });
