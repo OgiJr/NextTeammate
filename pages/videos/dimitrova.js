82efc3e23d051cae4a15712c2b723db1004f2752
Boris Radulov <sc20br@leeds.ac.uk>
2023-03-31 22:25:56 +0300

    email prep git push
    asd
    
    gitcommit

---
+++ pages/videos/dimitrova.js
@@ -0,0 +1,28 @@
+import Layout from "../../src/layout/Layout";
+import React from "react";
+import Footer from "../../src/layout/Footer";
+
+const SetPicture = () => {
+  return (
+    <Layout language={"en"}>
+      <div className="flex flex-row justify-center min-w-full">
+        <div className="flex flex-col justify-center min-w-full items-center justify-items-center">
+          <div
+            className="bg-gray-100 lg:w-[40vw] w-[80vw] py-8 mb-8 !border-sky-600 px-8"
+            style={{ borderWidth: 4, borderStyle: "solid", borderRadius: 20 }}
+          >
+            <div className="text-center min-w-full text-4xl font-semibold">Megi Dimitrova</div>
+            <div className="flex flex-col justify-center items-center">
+              <video className="w-[100%]" controls>
+                <source src={"/emails/dimitrova.mp4"} />
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </Layout>
+  );
+};
+
+export default SetPicture;
